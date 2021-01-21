567cda7e5b41bc45ac30ecc4ce0957ff1d1e19e8
Sam139 <77322528+Sam139@users.noreply.github.com>
2021-01-21 14:57:13 +0800

    Update hssp.js

--- scripts/hssp.js
+++ scripts/hssp.js
@@ -421,7 +421,7 @@ return new Promise((resolve, reject) => {
       else{
           message += '⚠️异常:'+result.err_tips+'\n'+'慢点看，不要慌，一会恢复\n'
           //let other = '⚠️异常:'+result.err_tips+'请重新获取readkey\n'
-          $.msg(jsname,'',other)
+          //$.msg(jsname,'',other)
           //return showmsg()
       }
           resolve()
